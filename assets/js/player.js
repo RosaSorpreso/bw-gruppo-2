471157fc265a927488d73569b2a997435331c624
RosaSorpreso <rosasorpreso@gmail.com>
2024-03-13 12:10:45 +0100

    push

---
+++ assets/js/player.js
@@ -0,0 +1,20 @@
+//Barra scorrimento duarata canzone
+/*function updateProgress() {
+    let progressBar = document.getElementById("myBar");
+    let progressSpan = document.querySelector(".mediaplayer span");
+    let currentTime = 0;
+    let totalTime = 50;
+
+    let progressInterval = setInterval(function() {
+        currentTime++;
+        let progressWidth = (currentTime / totalTime) * 100;
+        progressBar.style.width = progressWidth + "%";
+        progressSpan.textContent = currentTime;
+        if (currentTime >= totalTime) {
+            clearInterval(progressInterval);
+        }
+    }, 1000);
+}
+
+updateProgress();*/
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -39,24 +39,3 @@ fillAlbum(76635062);
 fillAlbum(75635062);
 fillAlbum(214959662);
 
-
-//Barra scorrimento duarata canzone
-function updateProgress() {
-    let progressBar = document.getElementById("myBar");
-    let progressSpan = document.querySelector(".mediaplayer span");
-    let currentTime = 0;
-    let totalTime = 50;
-
-    let progressInterval = setInterval(function() {
-        currentTime++;
-        let progressWidth = (currentTime / totalTime) * 100;
-        progressBar.style.width = progressWidth + "%";
-        progressSpan.textContent = currentTime;
-        if (currentTime >= totalTime) {
-            clearInterval(progressInterval);
-        }
-    }, 1000);
-}
-
-updateProgress();
-
