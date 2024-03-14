43d95be78811120d880601cfcf3fbca0250f69eb
RosaSorpreso <rosasorpreso@gmail.com>
2024-03-14 10:19:54 +0100

    progress bar e volume

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,144 +1,4 @@
-//Barra scorrimento duarata canzone
-/*function updateProgress() {
-    let progressBar = document.getElementById("myBar");
-    let progressSpan = document.querySelector(".mediaplayer span");
-    let currentTime = 0;
-    let totalTime = 50;
 
-    let progressInterval = setInterval(function() {
-        currentTime++;
-        let progressWidth = (currentTime / totalTime) * 100;
-        progressBar.style.width = progressWidth + "%";
-        progressSpan.textContent = currentTime;
-        if (currentTime >= totalTime) {
-            clearInterval(progressInterval);
-        }
-    }, 1000);
-}
-
-updateProgress();*/
-
-// let song = document.querySelector('#file-audio');
-// let playIcon = document.querySelector('#play-icon');
-// let progressed = document.querySelector('#myProgress');
-// let progressBar = document.querySelector('#myBar');
-// let currentDuration = document.querySelector('.current-duration');
-
-// //PROGRESS BAR// 
-
-// song.ontimeupdate = function () {
-//     progressed.style.width = Math.floor(song.currentTime * 100 / song.duration) + "%"
-// }
-// progressBar.addEventListener('click', (e) => {
-//     song.currentTime = ((e.offsetX / progressBar.offsetWidth) * song.duration)
-// });
-
-// //AGGIORNAMENTO TEMPO// 
-
-// let seconds = 0;
-// let mimutes = 0;
-// function formatTime(e) {
-//     let minutes = 0; let seconds = 0;
-
-//     for (let i = 0; i < e; i++) {
-//         seconds++; if (seconds >= 60) {
-//             minutes++; seconds = 0;
-//         }
-//     } return (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
-// } function aggiornamento() {
-//     currentDuration.innerText = formatTime(Math.floor(song.currentTime));
-// } setInterval(aggiornamento, 1000);
-
-// //PLAY/PAUSE// 
-
-// function playSong() {
-//     playIcon.classList.add('bi-pause-circle-fill')
-//     playIcon.classList.remove('bi-play-circle-fill')
-//     song.play()
-// }
-// function pauseSong() {
-//     playIcon.classList.remove('bi-pause-circle-fill')
-//     playIcon.classList.add('bi-play-circle-fill')
-//     song.pause()
-// } 
-
-// playIcon.addEventListener('click', () => {
-//     const songProd = playIcon.classList.contains('bi-play-circle-fill') 
-//     if (songProd) { playSong() } else {
-//         pauseSong()
-//     }
-// })
-
-//Barra scorrimento duarata canzone
-/*function updateProgress() {
-    let progressBar = document.getElementById("myBar");
-    let progressSpan = document.querySelector(".mediaplayer span");
-    let currentTime = 0;
-    let totalTime = 50;
-
-    let progressInterval = setInterval(function() {
-        currentTime++;
-        let progressWidth = (currentTime / totalTime) * 100;
-        progressBar.style.width = progressWidth + "%";
-        progressSpan.textContent = currentTime;
-        if (currentTime >= totalTime) {
-            clearInterval(progressInterval);
-        }
-    }, 1000);
-}
-
-updateProgress();*/
-
-// let song = document.querySelector('#file-audio');
-// let playIcon = document.querySelector('#play-icon');
-// let progressed = document.querySelector('#myProgress');
-// let progressBar = document.querySelector('#myBar');
-// let currentDuration = document.querySelector('.current-duration');
-
-// //PROGRESS BAR// 
-
-// song.ontimeupdate = function () {
-//     progressed.style.width = Math.floor(song.currentTime * 100 / song.duration) + "%"
-// }
-// progressBar.addEventListener('click', (e) => {
-//     song.currentTime = ((e.offsetX / progressBar.offsetWidth) * song.duration)
-// });
-
-// //AGGIORNAMENTO TEMPO// 
-
-// let seconds = 0;
-// let mimutes = 0;
-// function formatTime(e) {
-//     let minutes = 0; let seconds = 0;
-
-//     for (let i = 0; i < e; i++) {
-//         seconds++; if (seconds >= 60) {
-//             minutes++; seconds = 0;
-//         }
-//     } return (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
-// } function aggiornamento() {
-//     currentDuration.innerText = formatTime(Math.floor(song.currentTime));
-// } setInterval(aggiornamento, 1000);
-
-// //PLAY/PAUSE// 
-
-// function playSong() {
-//     playIcon.classList.add('bi-pause-circle-fill')
-//     playIcon.classList.remove('bi-play-circle-fill')
-//     song.play()
-// }
-// function pauseSong() {
-//     playIcon.classList.remove('bi-pause-circle-fill')
-//     playIcon.classList.add('bi-play-circle-fill')
-//     song.pause()
-// } 
-
-// playIcon.addEventListener('click', () => {
-//     const songProd = playIcon.classList.contains('bi-play-circle-fill') 
-//     if (songProd) { playSong() } else {
-//         pauseSong()
-//     }
-// })
 document.addEventListener("DOMContentLoaded", function() {
     let urlParams = new URLSearchParams(location.search);
     let albumId = urlParams.get('id');
@@ -146,7 +6,9 @@ document.addEventListener("DOMContentLoaded", function() {
     let song = document.querySelector('#file-audio');
     let playIcon = document.querySelector('#play-icon');
     let progressBar = document.querySelector('#myBar');
+    let bar = document.querySelector('#myProgress')
     let currentDuration = document.querySelector('.current-duration');
+    let volume = document.querySelector('.volume-bar')
 
     fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${albumId}`, {
         headers:{
@@ -167,17 +29,19 @@ document.addEventListener("DOMContentLoaded", function() {
             let number = trackElement.querySelector('.number');
             let streaming = trackElement.querySelector('.streaming');
             let songDuration = trackElement.querySelector('.song-duration');
+            let totalDuration = document.querySelector('.total-duration')
             
             title.innerText = track.title;
             artist.innerText = track.artist.name;
             streaming.innerText = track.rank;
             songDuration.innerText = createDuration(track.duration)
             number.innerText = index + 1;
-
+            
             title.addEventListener('click', () => {
                 titleBar.innerText = track.title
                 imgBar.src = album.cover_small
                 authorBar.innerText = album.artist.name
+                totalDuration.innerText = createDuration(track.duration)
                 song.src = track.preview;
                 playSong();
             });
@@ -193,9 +57,13 @@ document.addEventListener("DOMContentLoaded", function() {
         function padTo2Digits(num) {
         return num.toString().padStart(2, '0');
         }
-        const result = `${padTo2Digits(minutes)} min ${padTo2Digits(seconds)} sec`;
+        const result = `${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`;
         return result;
     }
+
+    volume.addEventListener("input", e => {
+        song.volume = e.currentTarget.value / 100;
+    })
 
     function playSong() {
         playIcon.classList.add('bi-pause-circle-fill');
@@ -229,7 +97,7 @@ document.addEventListener("DOMContentLoaded", function() {
         currentDuration.innerText = formatTime(song.currentTime);
     }
 
-    progressBar.addEventListener('click', (e) => {
-        song.currentTime = (e.offsetX / progressBar.offsetWidth) * song.duration;
-    });
+    bar.addEventListener('click', (e) => {
+        song.currentTime = (e.offsetX / bar.offsetWidth) * song.duration;
+    })
 });
