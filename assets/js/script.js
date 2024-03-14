328b8e18ab90ecbb90974b7cda6adf070be7dfb2
Luca Grossi <lucagrossi87@gmail.com>
2024-03-14 11:56:09 +0100

    dynamicTemplate

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,79 @@ function appendArtist (){
     iterateLoader()
 }
 
+function generateAlbum (albumId){
+    
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${albumId}`, {
+        headers:{
+            'Content-type':'application/json'
+        }
+    }).then(res => res.json())
+    .then(album => {
+        let coverAlbum = document.querySelector('#cover-album')
+        let type = document.querySelector('.type')
+        let title = document.querySelector('.album-title')
+        let imgArtist = document.querySelector('#artist-thumbnail')
+        let artistName = document.querySelector('#artist-name')
+        let year = document.querySelector('#album-year')
+        let totalSongs = document.querySelector('#songs-amount')
+        let totalTime = document.querySelector('#total-time')
+
+        coverAlbum.src = album.cover_medium
+        type.innerText = album.record_type
+        title.innerText = album.title
+        imgArtist.src = album.artist.picture
+        artistName.innerText = album.artist.name
+        year.innerText = album.release_date
+        totalSongs.innerText = album.nb_tracks + ' brani,'
+        totalTime.innerText = createDuration(album.duration)
+        
+        
+        //ciclo per inserire ogni canzone
+        for (i = 0; i < album.tracks.data.length; i++) {
+            let song = createClone()
+            
+            let number = song.querySelector('.number')
+            let songTitle = song.querySelector('.song-title')
+            let songArtist = song.querySelector('.song-artist')
+            let streaming = song.querySelector('.streaming')
+            let songDuration = song.querySelector('.song-duration')
+            let playedSong = document.querySelector('#played-song')
+            
+            number.innerText = i+1
+            songTitle.innerText = album.tracks.data[i].title
+            songArtist.innerText = album.tracks.data[i].artist.name
+            streaming.innerText = album.tracks.data[i].rank
+            songArtist.href = `artist-page.html?id=${album.artist.id}`
+            songDuration.innerText = createDuration(album.tracks.data[i].duration)
+            let appendSong = album.tracks.data[i]
+
+            songTitle.addEventListener('click', function(){
+                playedSong.src = appendSong.preview
+            })
+
+            document.querySelector('.target').append(song)
+        }
+        
+    })
+
+    function createClone (){
+        let template = document.querySelector('#song')
+        let clone = template.content.cloneNode(true)
+        return clone;
+    }
+
+    //funzione per impostare la durata di album/canzone in minuti e secondi
+    function createDuration (duration){
+        const minutes = Math.floor(duration / 60);
+        const seconds = duration % 60;
+        function padTo2Digits(num) {
+            return num.toString().padStart(2, '0');
+        }
+        const result = `${padTo2Digits(minutes)} min ${padTo2Digits(seconds)} sec`;
+        return result;
+    }
+}
+
 function appendAlbum (){
     let actualChild = document.getElementById('templates-field').getElementsByTagName('div')[0]
     document.getElementById('templates-field').removeChild(actualChild)
@@ -32,18 +105,21 @@ function iterateLoader () {
     for (let i = 0; i < document.getElementsByClassName('loadArtist').length; i++) {
         document.getElementsByClassName('loadArtist')[i].addEventListener('click', appendArtist);
     }
-
+    
     for (let i = 0; i < document.getElementsByClassName('loadHome').length; i++) {
         document.getElementsByClassName('loadHome')[i].addEventListener('click', appendHome);
     }
-
+    
     for (let i = 0; i < document.getElementsByClassName('loadAlbum').length; i++) {
+        let id = document.getElementsByClassName('loadAlbum')[i].albumId;
+        document.getElementsByClassName('loadAlbum')[i].addEventListener('click', function() {
+            generateAlbum(id)
+        });
         document.getElementsByClassName('loadAlbum')[i].addEventListener('click', appendAlbum);
     }
 }
 
 iterateLoader()
-
 
 function createClone (){
     let template = document.querySelector('#card-album')
@@ -69,7 +145,7 @@ const fillAlbum = idAlbum => {
         img.src = album.cover_big
         title.innerText = album.title
         artist.innerText = album.artist.name
-        // title.href = `album.html?id=${idAlbum}`
+        title.albumId = idAlbum
         // artist.href = `artist-page.html?id=${album.artist.id}`
 
         document.querySelector('.row-albums').append(card)
